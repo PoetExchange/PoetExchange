c3a11dbef6b389cb27d4b46e09ec9714b9692a1a
James Lott <jlott024@gmail.com>
2012-12-10 20:44:19 -0800

    Template work update Finished work on dept detail template, also created and finished work on a class detail template. Implemented breadcrumb navigation and began reorganization of CSS into object-oriented styling.

--- academics/urls.py
+++ academics/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('academics.views',
 #		url(r'^books/$', 'bookIndex'),
 #		url(r'^books/(?P<book_id>\d+)/$', 'bookDetail'),
-		url(r'^(?P<slug>[A-Z]{3,4})/$', 'deptDetail')
+		url(r'^(?P<slug>[A-Z]{3,4})/$', 'deptDetail'),
+		url(r'^[A-Z]{3,4}/(?P<slug>[a-z]{3,4}-[0-9]{3})/$','classDetail'),
 )
