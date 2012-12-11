8bb7f80a9fadc7a4062881511b3cc7144eda8a5b
James Lott <jlott024@gmail.com>
2012-12-10 20:44:19 -0800

    Template work update Finished work on dept detail template, also created and finished work on a class detail template. Implemented breadcrumb navigation and began reorganization of CSS into object-oriented styling.

--- academics/views.py
+++ academics/views.py
@@ -18,16 +18,35 @@ def bookDetail(request,book_id):
 	return render_to_response('academics/detail.html',{'book':b})
 
 def deptDetail(request, slug) :
-	uagent = whoAmI(request.META['HTTP_USER_AGENT'])
 	d = get_object_or_404(AcademicDepartment, dept_abrv = slug)
 	c = d.academicclass_set.all().order_by('class_number')
+	b = d.book_set.all().order_by('-entry_date')
 	context = {
 				'classes':c,
 				'dept':d,
-				'uagent':uagent,
+				'books':b,
 			}
 	return render_to_response(
-							'production/base_dept-detail.html', 
+							'production/base_deptDetail.html', 
 							context, 
 							context_instance=RequestContext(request)
 						)
+
+def classDetail(request, slug) :
+	c = get_object_or_404(AcademicClass, ac_slug=slug)
+	b = c.book_set.all().order_by('-entry_date')
+	dept_courses = c.class_dept.academicclass_set.all().order_by('class_number')
+	context = {
+			'class':c,
+			'books':b,
+			'dept':c.class_dept,
+			'dept_courses':dept_courses,
+			}
+	if c.class_professor :
+		prof_courses = c.class_professor.academicclass_set.all().order_by('class_number')
+		context['prof_courses']=prof_courses
+	return render_to_response(
+			'production/base_classDetail.html', 
+			context, 
+			context_instance=RequestContext(request),
+			)
